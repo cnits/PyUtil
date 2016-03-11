ca55c0f155a6b23738b8b32840fe84f1e7b86f30
root <appstoregugle2310@gmail.com>
2016-03-11 16:50:59 +0700

    update code for MongoDB Class

--- Lib/Mongo.py
+++ Lib/Mongo.py
@@ -2,11 +2,55 @@ from pymongo import MongoClient
 
 
 class MongoDB:
-    def __init__(self, host, port):
+    def __init__(self, db_name, host=None, port=None):
         if host is None or host == "":
             host = 'localhost'
         if port is None or port == "":
             port = 27017
-        self.dbm = MongoClient(host, port)
+        try:
+            self.dbm = MongoClient(host, port)[db_name]
 
+        except Exception as ex:
+            print(str(ex))
 
+    def find(self, collection, _filter):
+        if _filter is None:
+            _filter = {}
+        result = self.dbm[collection].find(_filter)
+        data = []
+        for i in result:
+            data.append(i)
+        return data
+
+    def find_one(self, collection, _filter):
+        data = self.dbm[collection].find_one(_filter)
+        return data
+
+    def save(self, collection, data, multiple=False):
+        if multiple is False:
+            return self.dbm[collection].insert_one(data)
+        else:
+            return self.dbm[collection].insert_many(data)
+
+    def update(self, collection, _filter, update, multiple=False, upsert=False):
+        if multiple is False:
+            return self.dbm[collection].update_one(_filter, update, upsert)
+        else:
+            return self.dbm[collection].update_many(_filter, update, upsert)
+
+    def delete(self, collection, _filter, multiple=False):
+        if multiple is False:
+            return self.dbm[collection].delete_one(_filter)
+        else:
+            return self.dbm[collection].delete_many(_filter)
+
+    def count(self, collection, _filter):
+        return self.dbm[collection].count(_filter)
+
+    def distinct(self, collection, key, _filter):
+        return self.dbm[collection].distinct(key, _filter)
+
+    def drop(self, collection):
+        self.dbm[collection].drop()
+
+    
--- Main.py
+++ Main.py
@@ -1,10 +1,24 @@
 from Lib.Curl import *
+from Lib.Mongo import *
+
+
+def mongo_test():
+    db = MongoDB("test", None, None)
+    # data = db.find('test', {'Id': 3})
+    # data = db.find_one('test', None)
+    data = db.distinct('test', "Name", None)
+    print(data)
+
+
+def curl_test():
+    # curl = Curl("http://pythonprogramming.net", {'s': 'basic', 'submit': 'search'})
+    curl = Curl("https://raw.githubusercontent.com/cnits/cnit/master/books.xml", None)
+    response = curl.do_request(None)
+    print(response)
+
 if __name__ == "__main__":
     try:
-        # curl = Curl("http://pythonprogramming.net", {'s': 'basic', 'submit': 'search'})
-        curl = Curl("https://raw.githubusercontent.com/cnits/cnit/master/books.xml", None)
-        response = curl.do_request(None)
 
-        print(response)
+        mongo_test()
     except Exception as ex:
         print(str(ex))
