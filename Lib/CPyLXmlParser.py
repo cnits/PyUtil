f5cb2a05eab967a9bb4fc18577a6100c76496f5d
phonglam <phonglam@ccintegration.com>
2016-04-05 18:10:58 +0700

    update code

---
+++ Lib/CPyLXmlParser.py
@@ -0,0 +1,25 @@
+from lxml import etree, builder, html
+from io import StringIO, BytesIO
+import os
+
+
+class CPyLXmlParser:
+    def __init__(self, xstr, encoding="utf-8"):
+        self.encoding = encoding
+        self.xstr = None
+        if xstr is None:
+            raise ValueError("Source content is none!")
+        else:
+            self.xstr = xstr
+
+    def parse(self, is_xml=True):
+        parser = etree.XMLParser()
+        if is_xml is False:
+            parser = etree.HTMLParser()
+        return etree.parse(self.xstr, parser)
+
+    def test(self):
+        pass
+
+    def get_element(self):
+        return ""
