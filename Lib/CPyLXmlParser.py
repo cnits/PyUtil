1476cbc35a59cf079380799ff355af1b4ab09652
phonglam <phonglam@ccintegration.com>
2016-04-06 15:05:46 +0700

    update code

--- Lib/CPyLXmlParser.py
+++ Lib/CPyLXmlParser.py
@@ -2,6 +2,41 @@ from lxml import etree, builder, html
 from io import StringIO, BytesIO
 import os
 
+'''
+HTML Element Methods
+HTML elements have all the methods that come with ElementTree, but also include some extra methods:
+	.drop_tree(): Drops the element and all its children. Unlike el.getparent().remove(el) this does
+not remove the tail text; with drop_tree the tail text is merged with the previous element.
+
+	.drop_tag(): Drops the tag, but keeps its children and text.
+	
+	.find_class(class_name): Returns a list of all the elements with the given CSS class name. Note that
+class names are space separated in HTML, so doc.find_class_name(’highlight’) will find an
+element like <div class="sidebar highlight">. Class names are case sensitive.
+
+	.find_rel_links(rel): Returns a list of all the <a rel="{rel}"> elements. E.g., doc.find_rel_links(’tag’)
+returns all the links marked as tags.
+
+	.get_element_by_id(id, default=None): Return the element with the given id, or the default if
+none is found. If there are multiple elements with the same id (which there shouldn’t be, but there often is),
+this returns only the first.
+
+	.text_content(): Returns the text content of the element, including the text content of its children, with no
+markup.
+
+	.cssselect(expr): Select elements from this element and its children, using a CSS selector expression.
+(Note that .xpath(expr) is also available as on all lxml elements.)
+
+	.label: Returns the corresponding <label> element for this element, if any exists (None if there is none).
+Label elements have a label.for_element attribute that points back to the element.
+
+	.base_url: The base URL for this element, if one was saved from the parsing. This attribute is not settable. Is
+None when no base URL was saved.
+
+	.classes: Returns a set-like object that allows accessing and modifying the names in the ’class’ attribute of
+the element. (New in lxml 3.5).
+
+'''
 
 class CPyLXmlParser:
     def __init__(self, xstr, encoding="utf-8"):
