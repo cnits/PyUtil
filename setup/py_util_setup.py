3a57eaf5b876033847f0a53b118e5a7ea73b0afb
phonglam <phonglam@ccintegration.com>
2016-04-27 16:30:04 +0700

    change file name and create setup file

--- setup/py_util_setup.py
+++ setup/py_util_setup.py
@@ -1,14 +1,18 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-
-setup(
-    name="CNIT-PyUtil",
-    version="0.0.1",
-    author="Phong VLam",
-    author_email="appstoregugle2310@gmail.com",
-    packages=["pyutil"],
-    include_package_data=True,
-    url="http://github.com/cnits/PyUtil.git",
-    description="Implement some of utility library for learning Python",
-    install_requires=["pymongo", "pycurl", "lxml"]
-)
+config = {
+    'name': "CNIT-PyUtil",
+    'description': "Implement some of utility library for learning Python",
+    'url': "http://github.com/cnits/PyUtil.git",
+    'author': "Phong VLam",
+    'author_email': "appstoregugle2310@gmail.com",
+    'packages': ["pyutil"],
+    'include_package_data': True,
+    'install_requires': ["pymongo", "pycurl", "lxml"],
+    'scripts': [],
+    'version': "0.0.1"
+}
+setup(**config)
